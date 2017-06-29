dca436041ed46299e772eb915bbe048874336eb7
Tenchez <Gray>
2017-06-29 08:52:25 -0400

    changed stats and prefabs

--- src/Assets/Scripts/UtilityConstants.cs
+++ src/Assets/Scripts/UtilityConstants.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class UtilityConstants
 {
-    public static readonly float INTERACT_RANGE = .3f;
+    public static readonly float INTERACT_RANGE = 1f;
 
     public static readonly int FRAMES_TILL_RELAX = 3;
 }
